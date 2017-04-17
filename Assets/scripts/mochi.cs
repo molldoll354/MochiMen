8064284b9a92a2afabf37f0a9018fe1ed2735a66
Unknown <njb350@nyu.edu>
2017-04-17 14:47:34 -0400

    4/17
    tried to do animation, doesnt really work yet

---
+++ Assets/scripts/mochi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mochi : MonoBehaviour {
+
+	public Animator mochiAnimatior;
+
+	// Use this for initialization
+	void Start () {
+		mochiAnimatior = GetComponent<Animator> ();
+		//mochiAnimatior.Play ("handSlap");
+		Invoke ("PlayAnimation", 2f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//mochiAnimatior.Play ("handSlap");
+	}
+
+	void PlayAnimation(){
+		mochiAnimatior.Play ("handSlap");
+		Debug.Log ("played");
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		//mochiAnimatior.Play ("handSlap");
+		Debug.Log ("should play");
+	}
+}
