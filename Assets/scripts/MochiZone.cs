86e82f078898b0811bf80ea6c889be649c87ec89
Molly <molly354@comcast.net>
2017-04-24 15:08:26 -0400

    score + timing work hallelujah

--- Assets/scripts/MochiZone.cs
+++ Assets/scripts/MochiZone.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class MochiZone : MonoBehaviour {
 
-	public GameObject mochi;
-	public GameObject hammer;
-	public GameObject hand;
+	//public GameObject mochi;
+	//public GameObject hammer;
+	//public GameObject hand;
 	public static int score;
 	public static int fails;
 	//public Text finishText;
@@ -19,7 +19,7 @@ public class MochiZone : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("Score: " + score + " Fails: " + fails);
+		//Debug.Log ("Score: " + score + " Fails: " + fails);
 
 		if (score == 10) {
 			//finishText.text = "You Win!";
