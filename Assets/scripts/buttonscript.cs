76efd0d04f448e30764edef6bd3747db79fe4d3c
jimistine <jrstine@me.com>
2017-04-24 15:15:46 -0400

    Added main menue, connected to test

---
+++ Assets/scripts/buttonscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class buttonscript : MonoBehaviour {
+
+	public Button playButton;
+	public Button aboutButton;
+	public Button controlsButton;
+	public Button quitButton;
+
+	public GameObject controlScreen;
+	public GameObject aboutScreen;
+
+
+	public void PlayScene(){
+		SceneManager.LoadScene ("dialogueTest");
+	}
+
+	public void ExitInstructions(){
+		controlScreen.SetActive (false);
+	}
+
+	public void OpenInstructions(){
+		controlScreen.SetActive (true);
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+
+	public void OpenAbout(){
+		aboutScreen.SetActive (true);
+	}
+
+	public void CloseAbout(){
+		aboutScreen.SetActive (false);
+	}
+}
