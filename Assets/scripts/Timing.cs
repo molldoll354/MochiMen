98fd96d91deb58d1d58906d7c09d13445679ab49
Unknown <njb350@nyu.edu>
2017-04-24 13:31:54 -0400

    timing

---
+++ Assets/scripts/Timing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timing : MonoBehaviour {
+
+	public float hammerHitTime;
+	public float handHitTime;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(){
+		
+	}
+
+}
