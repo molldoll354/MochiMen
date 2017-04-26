7f9e928363f3a89d8fe9392d1d9f1277b23320ba
jimistine <jrstine@me.com>
2017-04-26 13:54:48 -0400

    Menu stuffff

---
+++ Assets/scripts/buttonHoverController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class buttonHoverController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text Text;
+	public Color color;
+	public AudioClip sound;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Text.color = color; //Or however you do your color
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Text.color = Color.black; //Or however you do your color
+	}
+}
