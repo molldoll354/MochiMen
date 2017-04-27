f5b8d44b0a43da500c0661bd9c1391db63a3269b
jimistine <jrstine@me.com>
2017-04-26 20:11:12 -0400

    Finished the menu, just need controls text
    Will add controls text when we have controls :D

--- Assets/scripts/buttonHoverController.cs
+++ Assets/scripts/buttonHoverController.cs
@@ -6,17 +6,19 @@ using UnityEngine.UI;
 public class buttonHoverController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
 	public Text Text;
-	public Color color;
+	public Color hoverColor;
+	public Color returnColor;
 	public AudioClip sound;
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		Text.color = color; //Or however you do your color
+		Text.color = hoverColor; //Or however you do your color
+		Sound.me.PlaySound(sound, 1f);
 
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
 	{
-		Text.color = Color.black; //Or however you do your color
+		Text.color = returnColor; //Or however you do your color
 	}
 }
