b3fdf6d09711667af4e17992a8f15306586ef855
Molly <molly354@comcast.net>
2017-04-30 14:57:44 -0400

    hammer code is FIXED

--- Assets/scripts/hammerswing.cs
+++ Assets/scripts/hammerswing.cs
@@ -10,168 +10,48 @@ public class hammerswing : MonoBehaviour
 	public float maxAngleForward;
 	bool canSwing;
 	bool canRaise = true;
-
-	void Start ()
-	{
-		hammer = this.GetComponent<Transform> ();
-		canSwing = false;
-		canRaise = true;
-	}
-	
-	void FixedUpdate ()
-	{
-		if (canRaise == true) {
-			if (Input.GetKey (KeyCode.A)) {
-				//Debug.Log ("should raise");
-				if (hammer.eulerAngles.z < maxAngleBack + 1f || hammer.eulerAngles.z > maxAngleForward) {
-					transform.Rotate (0f, 0f, rotateSpeed * Time.deltaTime);
-					canSwing = true;
-				}
-			}
-		}
-
-		if (canSwing == true) {
-			if (Input.GetKey (KeyCode.S)) {
-				hammer.Rotate (0f, 0f, -1 * rotateSpeed * Time.deltaTime);
-				canRaise = false;
-			}
-		} 
-	}
-
-	void OnTriggerEnter2D (Collider2D coll)
-	{
-		//Debug.Log ("HIT");
-		if (coll.gameObject.CompareTag ("mochi")) {
-			canSwing = false;
-			canRaise = true;
-			//MochiZone.score++;
-		}
-	}
-//	void OnCollisionExit2D (Collision2D other){
-//		if(other.gameObject.CompareTag("hand")){
-//			MochiZone.fails++;
-//			Timing.score = 0;
-//		}
-//	}
-}
-
-public class hammerswing : MonoBehaviour
-{
-	Transform hammer;
-	public float rotateSpeed;
-	public float maxAngleBack;
-	public float maxAngleForward;
-	bool canSwing;
-	bool canRaise = true;
-
-	void Start ()
-	{
-		hammer = this.GetComponent<Transform> ();
-		canSwing = false;
-		canRaise = true;
-	}
-	
-	void FixedUpdate ()
-	{
-		if (canRaise == true) {
-			if (Input.GetKey (KeyCode.A)) {
-				//Debug.Log ("should raise");
-				if (hammer.eulerAngles.z < maxAngleBack + 1f || hammer.eulerAngles.z > maxAngleForward) {
-					transform.Rotate (0f, 0f, rotateSpeed * Time.deltaTime);
-					canSwing = true;
-				}
-			}
-		}
-
-		if (canSwing == true) {
-			if (Input.GetKey (KeyCode.S)) {
-				hammer.Rotate (0f, 0f, -1 * rotateSpeed * Time.deltaTime);
-				canRaise = false;
-			}
-		} 
-	}
-
-	void OnTriggerEnter2D (Collider2D coll)
-	{
-		//Debug.Log ("HIT");
-		if (coll.gameObject.CompareTag ("mochi")) {
-			canSwing = false;
-			canRaise = true;
-			//MochiZone.score++;
-		}
-	}
-//	void OnCollisionExit2D (Collision2D other){
-//		if(other.gameObject.CompareTag("hand")){
-//			MochiZone.fails++;
-//			Timing.score = 0;
-//		}
-//	}
-}
-
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class hammerswing : MonoBehaviour
-{
-	Transform hammer;
-	public float rotateSpeed;
-	public float maxAngleBack;
-	public float maxAngleForward;
-	bool canSwing;
-	bool canRaise = true;
-
 	Animator anim;
-
-	void Start ()
-	{
-		hammer = this.GetComponent<Transform> ();
-		canSwing = false;
-		canRaise = true;
-		anim = GetComponent<Animator> ();
-	}
-	
-	void FixedUpdate ()
-	{
-
-
-		anim.SetBool ("upswing", Input.GetKey (KeyCode.A));
-
+	void Start ()
+	{
+		hammer = this.GetComponent<Transform> ();
+		canSwing = false;
+		canRaise = true;
+		anim = GetComponent<Animator> ();
+	}
+	void FixedUpdate ()
+	{
+		anim.SetBool ("upswing", Input.GetKey (KeyCode.A));
 		anim.SetBool ("downswing", Input.GetKey (KeyCode.S));
-
-//		if (canRaise == true) {
-//			if (Input.GetKey (KeyCode.A)) {
-//				//Debug.Log ("should raise");
-//				if (hammer.eulerAngles.z < maxAngleBack + 1f || hammer.eulerAngles.z > maxAngleForward) {
-//					transform.Rotate (0f, 0f, rotateSpeed * Time.deltaTime);
-//					canSwing = true;
-//				}
-//			}
-//		}
-//
-//		if (canSwing == true) {
-//			if (Input.GetKey (KeyCode.S)) {
-//				hammer.Rotate (0f, 0f, -1 * rotateSpeed * Time.deltaTime);
-//				canRaise = false;
-//			}
-//		} 
-
-
-	}
-
-	void OnTriggerEnter2D (Collider2D coll)
-	{
-		//Debug.Log ("HIT");
-		if (coll.gameObject.CompareTag ("mochi")) {
-			canSwing = false;
-			canRaise = true;
-			//MochiZone.score++;
-		}
-	}
-//	void OnCollisionExit2D (Collision2D other){
-//		if(other.gameObject.CompareTag("hand")){
-//			MochiZone.fails++;
-//			Timing.score = 0;
-//		}
-//	}
+//		if (canRaise == true) {
+//			if (Input.GetKey (KeyCode.A)) {
+//				//Debug.Log ("should raise");
+//				if (hammer.eulerAngles.z < maxAngleBack + 1f || hammer.eulerAngles.z > maxAngleForward) {
+//					transform.Rotate (0f, 0f, rotateSpeed * Time.deltaTime);
+//					canSwing = true;
+//				}
+//			}
+//		}
+//
+//		if (canSwing == true) {
+//			if (Input.GetKey (KeyCode.S)) {
+//				hammer.Rotate (0f, 0f, -1 * rotateSpeed * Time.deltaTime);
+//				canRaise = false;
+//			}
+//		} 
+	}		
+	void OnTriggerEnter2D (Collider2D coll)
+	{
+		//Debug.Log ("HIT");
+		if (coll.gameObject.CompareTag ("mochi")) {
+			canSwing = false;
+			canRaise = true;
+			//MochiZone.score++;
+		}
+	}
+//	void OnCollisionExit2D (Collision2D other){
+//		if(other.gameObject.CompareTag("hand")){
+//			MochiZone.fails++;
+//			Timing.score = 0;
+//		}
+//	}
 }
