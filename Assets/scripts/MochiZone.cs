5ebd25f62552ee51d2dcae1f0cde57b3c18441f1
Unknown <njb350@nyu.edu>
2017-04-30 22:17:53 -0400

    4/30
    mochi zone added, score UI added, image fade works, SFX on hammer and hand hits, background sound, crane isnt working yet, game won state added

--- Assets/scripts/MochiZone.cs
+++ Assets/scripts/MochiZone.cs
@@ -10,22 +10,40 @@ public class MochiZone : MonoBehaviour {
 	//public GameObject hand;
 	public static int score;
 	public static int fails;
-	//public Text finishText;
-	// Use this for initialization
+	public Image mochiZoneImage;
+	public float mochiZoneImageFadeSpeed;
+	public AudioClip mochiZoneSound;
+	public Text finishText;
+
 	void Start () {
 		score = 0;
 		fails = 0;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		//Debug.Log ("Score: " + score + " Fails: " + fails);
-
 		if (score == 10) {
-			//finishText.text = "You Win!";
+			// now in mochi zone
+			Camera.main.gameObject.GetComponent<AudioSource>().clip = mochiZoneSound;
+			Camera.main.gameObject.GetComponent<AudioSource> ().Play ();
+			StartCoroutine ("fadeImageIn");
 		}
+		if (score == 20){
+			finishText.gameObject.SetActive (true);
+			Time.timeScale = 0;
+		}
+
+		/*
 		if (fails == 3) {
-			//finishText.text = "You Lose!";
 		}
+		*/
 	}
+
+	IEnumerator fadeImageIn(){
+		while(mochiZoneImage.color.a < 1f){
+			mochiZoneImage.color = new Color (mochiZoneImage.color.r, mochiZoneImage.color.g, mochiZoneImage.color.b, mochiZoneImage.color.a + mochiZoneImageFadeSpeed * Time.deltaTime);
+			yield return new WaitForFixedUpdate();
+		}
+
+	}
+
 }
